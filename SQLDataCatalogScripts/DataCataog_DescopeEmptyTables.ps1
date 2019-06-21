d6dd088eaeb0ca725bd3f52714ea5944484f3fd8
josh smith <toosuto@gmail.com>
2019-06-21 07:41:11 -0700

    The One with the Initial Files
    30 minute intro talk slides and SQL Data Catalog PS scripts.

---
+++ SQLDataCatalogScripts/DataCataog_DescopeEmptyTables.ps1
@@ -0,0 +1,38 @@
+<#
+    Script will pull out column data classifications and apply it to what should be the same
+    database in a second location.
+
+    Created: 2018-12-06
+
+    Author: Josh Smith
+
+    Changelog:
+        2019-02-12  Refactored to use the new data catalog PS module. No more querying the source or target database! JDS
+
+#>
+
+$dataCatalog = 'http://sqlavengers.stcu.local:15156/'
+$sqlinstance = 'sqlavengers\hawkeye' #Read-Host -Prompt 'Please enter SQL instance with the target database.'
+$database = 'WideWorldImportersDW' #Read-Host -Prompt 'Enter the target database name'
+
+
+##################################################################################################
+$AuthToken = 'NTg2MTYxODI4NDY4MzU5MTY4OjRkZjQ4YTFhLWM4NTMtNDY3MS1iMDE1LTc1MmMxOWY1ZGUzYw==' 
+$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$Headers.Add("Authorization", "Bearer $AuthToken")
+
+# refresh the PS module for Data catalog and store the auth token:
+$cmd = $dataCatalog + 'powershell'
+$outfile = 'C:\Program Files\WindowsPowerShell\Modules\data-calog.psm1'
+Invoke-WebRequest -Uri $cmd -OutFile $outfile -Headers @{"Authorization"="Bearer $AuthToken"; "Accept"="text/plain"}
+Import-Module $outfile
+
+Use-Classification -ClassificationAuthToken $AuthToken
+
+$emptyCols = Get-Columns -instanceName $sqlinstance -databaseName $database | Where-Object {$_.tableRowCount  -eq 0}
+
+$tags = @{
+    "Scope" = @("Descoped: Empty Table")
+}
+
+Import-ColumnsTags -columns $emptyCols -categories $tags
---
+++ SQLDataCatalogScripts/DataCataog_TagIntIDs.ps1
@@ -0,0 +1,27 @@
+
+$dataCatalog = 'http://sqlavengers.stcu.local:15156/'
+$sqlinstance = 'sqlavengers\hawkeye' #Read-Host -Prompt 'Please enter SQL instance with the target database.'
+$database = 'WideWorldImportersDW' #Read-Host -Prompt 'Enter the target database name'
+
+
+##################################################################################################
+$AuthToken = 'NTg2MTYxODI4NDY4MzU5MTY4OjRkZjQ4YTFhLWM4NTMtNDY3MS1iMDE1LTc1MmMxOWY1ZGUzYw==' 
+$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$Headers.Add("Authorization", "Bearer $AuthToken")
+
+# refresh the PS module for Data catalog and store the auth token:
+$cmd = $dataCatalog + 'powershell'
+$outfile = 'C:\Program Files\WindowsPowerShell\Modules\data-calog.psm1'
+Invoke-WebRequest -Uri $cmd -OutFile $outfile -Headers @{"Authorization"="Bearer $AuthToken"; "Accept"="text/plain"}
+Import-Module $outfile
+
+Use-Classification -ClassificationAuthToken $AuthToken
+
+$catCols = Get-Columns -instanceName $sqlinstance -databaseName $database | Where-Object {$_.columnName  -like "*ID" -and $_.dataType -eq 'int'}
+
+$tags = @{
+    "Sensitivity" = @("General")
+    "Information Type" = @("Other")
+}
+
+Import-ColumnsTags -columns $catCols -categories $tags
