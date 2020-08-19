1501b5884467977a2a752184d3d7e38ef7174405
josh smith <joshs@stcu.org>
2020-08-18 17:04:55 -0700

    The one with slide updates and cleanup fixes

--- SQLSecurity/envCleanup.sql
+++ SQLSecurity/envCleanup.sql
@@ -3,7 +3,7 @@ IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'testdbCreator')
 	DROP LOGIN [testdbCreator];
 
 IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'Broseph')
-	DROP LOGIN [testdbCreator];
+	DROP LOGIN [Broseph];
 
 IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'crole_Troubleshooting')
 	DROP SERVER ROLE [crole_Troubleshooting];
@@ -14,5 +14,5 @@ IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'crole_CreateDB')
 IF EXISTS (SELECT * FROM sys.databases WHERE [name] = 'neverDrop')
 	DROP DATABASE neverDrop;
 
-IF EXISTS (SELECT * FROM sys.databases WHERE [name] = 'sqfeToDrop')
+IF EXISTS (SELECT * FROM sys.databases WHERE [name] = 'safeToDrop')
 	DROP DATABASE safeToDrop;
