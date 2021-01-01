896983ef014bb4a139b2a8622e77a71d828a12b2
josh smith <josh@accitentionaldba.com>
2021-01-01 11:28:12 -0800

    fixed cleanup script, cleaned up demo script

--- SQLSecurity/envCleanup.sql
+++ SQLSecurity/envCleanup.sql
@@ -1,95 +1,134 @@
 /* Pre-Demo Cleanup */
 IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'Rona')
-	DROP LOGIN [Rona]
+BEGIN
+	ALTER LOGIN Rona REVOKE ALTER ANY LOGIN;
 
-ALTER ROLE sysAdmin DROP MEMBER Rona
-CREATE LOGIN Rona WITH PASSWORD = 'myAwesomePassword'
+    ALTER SERVER ROLE sysAdmin DROP MEMBER Rona;
+
+    DROP LOGIN [Rona]
+END
 
 IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'Jaime')
-	DROP LOGIN [Jaime]
-
-CREATE LOGIN Jaime WITH PASSWORD = 'yourAwesomePassword'
+    DROP LOGIN [Jaime]
 
 IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'TKtheFish')
-	DROP LOGIN [TKtheFish];
+    DROP LOGIN [TKtheFish];
 
-IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'myOtherNewLogin')
-	DROP LOGIN [myOtherNewLogin];
+IF EXISTS
+(
+    SELECT *
+    FROM sys.server_principals
+    WHERE [name] = 'myOtherNewLogin'
+)
+    DROP LOGIN [myOtherNewLogin];
 
-IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'testdbCreator')
-	DROP LOGIN [testdbCreator];
+IF EXISTS
+(
+    SELECT *
+    FROM sys.server_principals
+    WHERE [name] = 'testdbCreator'
+)
+    DROP LOGIN [testdbCreator];
 
 IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'chad')
-	DROP LOGIN [chad];
+    DROP LOGIN [chad];
 
 IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'SusanInHR')
-	DROP LOGIN [SusanInHR];
+    DROP LOGIN [SusanInHR];
 
-IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'crole_Troubleshooting')
-	DROP SERVER ROLE [crole_Troubleshooting];
+IF EXISTS
+(
+    SELECT *
+    FROM sys.server_principals
+    WHERE [name] = 'crole_Troubleshooting'
+)
+    DROP SERVER ROLE [crole_Troubleshooting];
 
-IF EXISTS (SELECT * FROM sys.server_principals WHERE [name] = 'crole_CreateDB')
-	DROP SERVER ROLE [crole_CreateDB];
+IF EXISTS
+(
+    SELECT *
+    FROM sys.server_principals
+    WHERE [name] = 'crole_CreateDB'
+)
+    DROP SERVER ROLE [crole_CreateDB];
 
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE [name] = 'neverDrop')
-	CREATE DATABASE neverDrop;
+    CREATE DATABASE neverDrop;
 
 IF EXISTS (SELECT * FROM sys.databases WHERE [name] = 'safeToDrop')
-	DROP DATABASE safeToDrop;
-
+    DROP DATABASE [safeToDrop];
 USE WideWorldImporters;
 GO
 
 EXEC sp_changedbowner 'sa'
 
+IF EXISTS
+(
+    SELECT *
+    FROM sys.database_principals
+    WHERE [name] = 'TKTheFish'
+)
+    DROP USER [TKTheFish];
+
 IF EXISTS (SELECT * FROM sys.database_principals WHERE [name] = 'Chad')
 BEGIN
-	REVOKE SHOWPLAN TO Chad;
-	DROP USER [Chad];
+    REVOKE SHOWPLAN TO Chad;
+    DROP USER [Chad];
 END
 GO
 
 IF EXISTS (SELECT * FROM sys.database_principals WHERE [name] = 'Rona')
 BEGIN
-	ALTER ROLE db_owner DROP MEMBER Rona;
-	ALTER ROLE db_reader DROP MEMBER Rona;
+    ALTER ROLE db_owner DROP MEMBER Rona;
+    ALTER ROLE db_reader DROP MEMBER Rona;
 
-	REVOKE SELECT ON Sales.Orders TO Rona;
-	DROP USER [Rona];
+    REVOKE SELECT ON Sales.Orders TO Rona;
+    DROP USER [Rona];
 END
 
 
 IF EXISTS (SELECT * FROM sys.database_principals WHERE [name] = 'Jaime')
-BEGIN
-	DROP USER [Jaime];
-END
+    DROP USER [Jaime];
 
 USE AdvWorksDWViews;
 GO
 
-IF EXISTS (SELECT * FROM sys.database_principals WHERE [name] = 'SusanInHR')
+IF EXISTS
+(
+    SELECT *
+    FROM sys.database_principals
+    WHERE [name] = 'SusanInHR'
+)
 BEGIN
-	ALTER ROLE crole_StandardReports DROP MEMBER [SusanInHr]
-	ALTER ROLE crole_SensitiveReports DROP MEMBER [SusanInHr]
-	DROP USER [SusanInHR];
+    ALTER ROLE crole_StandardReports DROP MEMBER [SusanInHr]
+    ALTER ROLE crole_SensitiveReports DROP MEMBER [SusanInHr]
+    DROP USER [SusanInHR];
 END
 GO
 
 IF EXISTS (SELECT * FROM sys.database_principals WHERE [name] = 'Chad')
 BEGIN
-	ALTER ROLE crole_StandardReports DROP MEMBER [Chad]
-	DROP USER [Chad];
+    ALTER ROLE crole_StandardReports DROP MEMBER [Chad]
+    DROP USER [Chad];
 END
 GO
 
-
-
-IF EXISTS (SELECT * FROM sys.database_principals WHERE [name] = 'crole_StandardReports')
-	DROP ROLE [crole_StandardReports];
+IF EXISTS
+(
+    SELECT *
+    FROM sys.database_principals
+    WHERE [name] = 'crole_StandardReports'
+)
+    DROP ROLE [crole_StandardReports];
 GO
 
-IF EXISTS (SELECT * FROM sys.database_principals WHERE [name] = 'crole_SensitiveReports')
-	DROP ROLE [crole_SensitiveReports];
+IF EXISTS
+(
+    SELECT *
+    FROM sys.database_principals
+    WHERE [name] = 'crole_SensitiveReports'
+)
+    DROP ROLE [crole_SensitiveReports];
 GO
 
 EXEC sp_configure 'Show advanced Options', 1;
@@ -98,5 +137,3 @@ RECONFIGURE
 EXEC sp_configure 'max degree of parallelism', 2;
 EXEC sp_configure 'show advanced options', 0;
 RECONFIGURE;
-
-
